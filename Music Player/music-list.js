06b2be96a07fd2cfd8d7ac93049835e869be59c2
Ajay Jha <ajayhp245@gmail.com>
2021-10-30 14:33:23 +0530

    Add files via upload

---
+++ Music Player/music-list.js
@@ -0,0 +1,22 @@
+
+let allMusic = [
+    {
+      name: "Symphony",
+      artist: "Arc North",
+      img: "music-1",
+      src: "music-1"
+    },
+    {
+      name: "Time To Talk",
+      artist: "Asena",
+      img: "music-2",
+      src: "music-2"
+    },
+    {
+      name: "Wounds [NCS Release]",
+      artist: "Papa Khan",
+      img: "music-3",
+      src: "music-3"
+    },
+   
+  ];
